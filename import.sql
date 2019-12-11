64090a659b5d4fc3958fa5941bdb11793463219f
Elise Miller <elise.miller168@gmail.com>
2019-12-10 23:43:02 -0600

    uploaded data incorrectly on last push; corrected

--- import.sql
+++ import.sql
@@ -1,3 +1,7 @@
+truncate player_stats cascade;
+truncate player_income cascade;
+truncate area_income  cascade;
+
 \copy player_stats from 'resources/player_stats.csv' csv delimiter ',' header;
 \copy player_income from 'resources/player_salary_cleaned.csv' csv delimiter ',' header;
 \copy area_income from 'resources/clean_income.csv' csv delimiter ',' header;
---
+++ queries.sql
@@ -0,0 +1,2 @@
+SELECT team FROM player_stats 
+JOIN player_income ON player_stats.name = player_income.name;
