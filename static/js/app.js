3acc7cf162ec4bf5cfeaa17a49bbbdf1eaadef25
Elise Miller <elise.miller168@gmail.com>
2019-12-15 20:22:38 -0600

    working on bar chart

---
+++ static/js/app.js
@@ -0,0 +1,18 @@
+d3.json("/api/map_data").then(map_data => {
+  console.log('This worked');
+  console.log(map_data);
+
+  var lat = map_data.map(row => row.lat);
+  var test = map_data.map(row => LatLng(row.lat, row.long))
+  console.log(lat)
+});
+
+d3.json("/api/player_income_data").then(player_income_data => {
+  console.log('This worked');
+  console.log(player_income_data);
+});
+
+d3.json("/api/team_income_data").then(team_income_data => {
+  console.log('This worked');
+  console.log(team_income_data);
+});
