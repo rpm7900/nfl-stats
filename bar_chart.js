eeaef4f84a375a9fce2e6c30f66898b317063eb8
Elise Miller <elise.miller168@gmail.com>
2019-12-16 20:00:59 -0600

    save changes

---
+++ bar_chart.js
@@ -0,0 +1,54 @@
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// set the dimensions and margins of the graph
+var chartMargin = {
+    top: 20,
+    right: 30,
+    bottom: 30,
+    left: 80
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from hours-of-tv-watched.csv
+d3.json("/api/player_income_data").then(player_income_data => {
+
+  // Print the tvData
+  console.log(player_income_data);
+
+  var barSpacing = 10; // desired space between each bar
+  var scaleY = 10; // 10x scale on rect height
+
+  // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
+  var barWidth = (chartWidth - (barSpacing * (player_income_data.length - 1))) / player_income_datax.length;
+
+  // @TODO
+  // Create code to build the bar chart using the tvData.
+  chartGroup.selectAll(".bar")
+    .data(player_income_data)
+    .enter()
+    .append("rect")
+    .classed("bar", true)
+    .attr("width", d => barWidth)
+    .attr("height", d => d.cap_hit * scaleY)
+    .attr("x", (d, i) => i * (barWidth + barSpacing))
+    .attr("y", d => chartHeight - d.cap_hit * scaleY);
+}).catch(function(error) {
+  console.log(error);
+});
